22741d0507413038da83e1a87576b93298e4a661
samian2002 <samiantariq14@gmail.com>
2024-04-23 16:34:38 +0200

    creazione ciclo

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//Ul dove verranno inseriti i risultati
+const outputList = document.getElementById('output');
+//Ciclo da 1 a 100
+for (let n = 1; n <= 100; n**) {
+    // Stringa per memorizzare output
+    let output = " ";
+    
+}
