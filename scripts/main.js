2fec450d6b103a605ee9f5f0fa715e12894c9bb2
samian2002 <samiantariq14@gmail.com>
2024-04-23 16:57:23 +0200

    creo li

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,27 @@
-//Ul dove verranno inseriti i risultati
+// Seleziona l'elemento ul dove verranno inseriti i risultati
 const outputList = document.getElementById('output');
-//Ciclo da 1 a 100
-for (let n = 1; n <= 100; n**) {
-    // Stringa per memorizzare output
-    let output = " ";
-    // Controlli
-    if (n % 3 === 0) {
-        output += "Fizz";
-    }
-    if (n % 5 === 0) {
-        output += "Buzz";
-    }
-    // Output vuoto
-    if (output === " ") {
-        output = n;
-    }
-    
+
+// Ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  //Stringa vuota per memorizzare output
+  let output = '';
+
+  // Controlli
+  if (i % 3 === 0) {
+    output += 'Fizz';
+  }
+  if (i % 5 === 0) {
+    output += 'Buzz';
+  }
+
+  // Output vuoto
+  if (output === '') {
+    output = i;
+  }
+
+  // Crea un elemento li
+  const listItem = document.createElement('li');
+  listItem.textContent = output;
+  outputList.appendChild(listItem);
 }
+
