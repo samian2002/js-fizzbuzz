59464f47f14b07e4f9f55883705033eead52e82f
samian2002 <samiantariq14@gmail.com>
2025-02-17 02:41:39 +0100

    controllo multipli di 3 e 5

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //Ciclo i numeri da 1 a 100
 for (let i = 1; i <= 100; i ++) {
-    
+    //Controllo che il numero sia multimo sia di 3 che di 5
+    //Devo verificare che dividendno per 3 e per 5 in resto sia 0, (controllo che entrmbi i test siano veri con &&)
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+
 }
